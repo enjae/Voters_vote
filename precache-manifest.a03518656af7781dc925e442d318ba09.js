3cc67f0bff534b8d692abe680d9d441f18a46184
enjae <jarialnandini1911@gmail.com>
2021-06-29 13:15:42 +0530

    Updates

---
+++ precache-manifest.a03518656af7781dc925e442d318ba09.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461548818e56fc7107dc3db77b53cafe",
+    "url": "/Voters_vote/index.html"
+  },
+  {
+    "revision": "a83d6fcb4e1fa8ce50c4",
+    "url": "/Voters_vote/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "79f7bb2aa1f07eead4c7",
+    "url": "/Voters_vote/static/css/main.b8b6a3a5.chunk.css"
+  },
+  {
+    "revision": "a83d6fcb4e1fa8ce50c4",
+    "url": "/Voters_vote/static/js/2.baa46404.chunk.js"
+  },
+  {
+    "revision": "4c3330dcc82e3d2cbb0f08ee76f63d6d",
+    "url": "/Voters_vote/static/js/2.baa46404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f7bb2aa1f07eead4c7",
+    "url": "/Voters_vote/static/js/main.29671efe.chunk.js"
+  },
+  {
+    "revision": "3cf06d5c7ea022a7576f",
+    "url": "/Voters_vote/static/js/runtime-main.0669031c.js"
+  },
+  {
+    "revision": "232a287703be951fdc0c4d394fa0ea1b",
+    "url": "/Voters_vote/static/media/evmf1.232a2877.png"
+  },
+  {
+    "revision": "6ee0a19899f83df193eee4d6824db050",
+    "url": "/Voters_vote/static/media/evmf2.6ee0a198.png"
+  },
+  {
+    "revision": "d853920f7aaf84621c98411a79944a9b",
+    "url": "/Voters_vote/static/media/evmf3.d853920f.png"
+  },
+  {
+    "revision": "bae05183ff65c42c9468e8042f6035fc",
+    "url": "/Voters_vote/static/media/evmf4.bae05183.png"
+  },
+  {
+    "revision": "12912dd4a6247f43bb048bb791cce7ac",
+    "url": "/Voters_vote/static/media/evmf5.12912dd4.png"
+  }
+]);
